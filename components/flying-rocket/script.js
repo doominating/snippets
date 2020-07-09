409436b01647ce4299cb901d99642cac2fca1e6f
Afonso Amaro <doominating@gmail.com>
2020-07-09 17:44:10 -0300

    add flying rocket

---
+++ components/flying-rocket/script.js
@@ -0,0 +1,22 @@
+function stars() {
+  let count = 50;
+  let scene = document.querySelector('.scene');
+  let i = 0;
+  while (i < count) {
+    let star = document.createElement('i');
+    let x = Math.floor(Math.random() * window.innerWidth);
+
+    let duration = Math.random() * 2;
+    let h = Math.random() * 100;
+
+    star.style.left = x + 'px';
+    star.style.width = 1 + 'px';
+    star.style.height = 25 + h + 'px';
+    star.style.animationDuration = duration + 's';
+    star.style.zIndex = 0;
+
+    scene.appendChild(star)
+    i++;
+  }
+}
+stars();
